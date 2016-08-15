958c6d76914d59b2bc89c199e0e74eedf0583d76
MrKiaN <Mrkian.s2014@gmail.com>
2016-08-15 17:03:00 +0430

    Update foroshgah.lua

--- plugins/foroshgah.lua
+++ plugins/foroshgah.lua
@@ -1,37 +1,35 @@
-local function set_description(msg, data)
+local function set_shop(msg, data)
     if not is_momod(msg) then
         return "این امکان برای شما موجود نیست!"
     end
-    local data_cat = 'description'
-    data[tostring(msg.to.id)][data_cat] = deskripsi
+    local data_cat = 'shop'
+    data[data_cat] = deskripsi
     save_data(_config.moderation.data, data)
 
     return 'Set shop item to:\n'..deskripsi
 end
 
-local function get_description(msg, data)
-    local data_cat = 'description'
-    if not data[tostring(msg.to.id)][data_cat] then
+local function get_shop(msg, data)
+    local data_cat = 'shop'
+    if not data[data_cat] then
         return 'No item available.'
     end
-    local about = data[tostring(msg.to.id)][data_cat]
+    local about = data[data_cat]
     local about = string.gsub(msg.to.print_name, "_", " ")..':\n\n'..about
     return 'About '..about
 end
 
 function run(msg, matches)
-	local data = load_data(_config.moderation.data)
+ local data = load_data(_config.moderation.data)
     local receiver = get_receiver(msg)
-    if is_chat_msg(msg) then
-        if data[tostring(msg.to.id)] then
-        	local settings = data[tostring(msg.to.id)]['settings']
-        	local get_cmd = matches[1]
+        if data['shop'] then
+         local get_cmd = matches[1]
            if matches[1] == 'setshop' and matches[2] then
                 deskripsi = matches[2]
-                return set_description(msg, data)
+                return set_shop(msg, data)
            end
             if matches[1] == 'shop' then
-                return get_description(msg, data)
+                return get_shop(msg, data)
             end
         end
     end
@@ -47,7 +45,6 @@ return {
 
   }, 
   run = run,
-  pre_process = pre_process
 }
 
 end
