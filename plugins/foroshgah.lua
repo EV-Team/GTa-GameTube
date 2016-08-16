a8d200f9c3ce098122b9977b3dba0c1c82990254
MrKiaN <Mrkian.s2014@gmail.com>
2016-08-16 15:47:44 +0430

    first commit

--- plugins/foroshgah.lua
+++ plugins/foroshgah.lua
@@ -1,50 +1,57 @@
-local function set_shop(msg, data)
-    if not is_momod(msg) then
-        return "این امکان برای شما موجود نیست!"
-    end
-    local data_cat = 'shop'
-    data[data_cat] = deskripsi
-    save_data(_config.moderation.data, data)
+ do
+local function save_value(msg, name, value)
+  if (not name or not value) then
+    return 
+  end
+  local hash = nil
+  if msg.to.type == 'chat' or 'channel' then
+    hash = 'botcommand:variables'
+  end
+  if hash then
+    redis:hset(hash, name, value)
+    return "Done!"
+  end
+end
+local function get_variables_hash(msg)
+  if msg.to.type == 'chat' or 'channel' then
+    return 'botcommand:variables'
+  end
+end 
 
-    return 'Set shop item to:\n'..deskripsi
+local function get_value(msg, var_name)
+  local hash = get_variables_hash(msg)
+  if hash then
+    local value = redis:hget(hash, var_name)
+    if not value then
+      return
+    else
+      return value
+    end
+  end
 end
 
-local function get_shop(msg, data)
-    local data_cat = 'shop'
-    if not data[data_cat] then
-        return 'No item available.'
-    end
-    local about = data[data_cat]
-    local about = string.gsub(msg.to.print_name, "_", " ")..':\n\n'..about
-    return 'About '..about
+local function run(msg, matches)
+if matches[1]:lower() == 'setcommand' then
+  if not is_momod(msg) then
+    return "این امکان برای شما موجود نیستℹ"
+  end
+  local name = string.sub(matches[2], 1, 50)
+  local value = string.sub(matches[3], 1, 1000)
+  local name1 = user_print_name(msg.from)
+  local text = save_value(msg, name, value)
+  return text
 end
-
-function run(msg, matches)
- local data = load_data(_config.moderation.data)
-    local receiver = get_receiver(msg)
-        if data['shop'] then
-         local get_cmd = matches[1]
-           if matches[1] == 'setshop' and matches[2] then
-                deskripsi = matches[2]
-                return set_shop(msg, data)
-           end
-            if matches[1] == 'shop' then
-                return get_shop(msg, data)
-            end
-        end
-    end
+ if matches[1] == '!' or '/' or '#' then
+    return get_value(msg, matches[2])
+  else
+    return
+  end
 end
-
 return {
-  description = "",
-  usage = {},
   patterns = {
-    "^/(setshop) (.+)$",
-    "^/(shop)$",
-
-
+   "^[!/#](setcommand) ([^%s]+) (.+)$",
+   "^([!/#])(.+)$",
   }, 
-  run = run,
+  run = run 
 }
-
 end
