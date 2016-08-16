a02c74931ab138dba87bebfac48b3a3decfa8f29
MrKiaN <Mrkian.s2014@gmail.com>
2016-08-16 18:39:38 +0430

    first commit

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,45 +1,28 @@
 local function run(msg, matches)
   if is_chat_msg(msg) then
-    local text = [[‌✅Commands to lock|unlock
-💭/close|open link
-💭/close|open member
-💭/close|open name
-💭/close|open bot
-💭/close|open photo
-💭/close|open sticker
-💭/close|open file 
-💭/close|open audio
+    local text = [[‌✅Commands to play game
+برای نشان دادن پول خود:
+اعتبار (نام خود)
 
-➕
-✅Commands for control member
-💭/kick : by <reply|id|username>
-💭/ban : by <reply|id|username>
-💭/unban : by <reply|id|username>
-💭/kickme
+🔷
 
-➕
-✅Group control 
-💭/rules
-💭/setrules <write rules>
-💭/about
-💭/setabout <write about>
-💭/setphoto : then send photo
-💭/setname <write name>
-💭/id
-💭/id chat
-💭/group settings 
-💭/getlink : send link in your pv
-💭/relink <idchat> : change link group and send new link your pv
-💭/modlist
-💭/help
+برای نشان دادن فروشگاه:
+shop فروشگاه یا
 
-➕
-✅ Group Promote  commands
-💭/spromote : up leader by <reply|id|username> 
-💭/sdemote : in promote by <reply|id|username>
-💭/promote : by <reply|id|username> 
-💭/demote : by <reply|id|username> 
+🔷
 
+/news
+برای اطلاع از آخرین خبر ها
+
+🔷
+
+/helpfun
+برای نمایش دیگر امکانات ربات
+
+برای آشنایی از دیگر دستورات در کانال زیر جوین شوید:
+@GTASanGame
+
+شما می توانید بخ جای / از ! و # استفاده کنید
 ➖🔸➖🔹➖🔸➖🔹➖]]
     return text
   end
@@ -99,7 +82,7 @@ return {
     "!help: Show list of plugins.",
   },
   patterns = {
-    "^/(help)$",
+    "^[/!#](help)$",
   }, 
   run = run,
 }
